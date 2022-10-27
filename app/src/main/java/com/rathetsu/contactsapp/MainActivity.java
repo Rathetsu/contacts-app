32f32a2890567b6e271b76b3cf8999e86c787af6
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2022-10-27 03:20:58 +0200

    created new activity for creating and editing contacts

--- app/src/main/java/com/rathetsu/contactsapp/MainActivity.java
+++ app/src/main/java/com/rathetsu/contactsapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.rathetsu.contactsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddEditContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
