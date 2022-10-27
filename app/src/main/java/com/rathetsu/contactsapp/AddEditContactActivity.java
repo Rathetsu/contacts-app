62724d3f3d9bac2b1988939a1bed131563967dfb
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2022-10-27 04:51:47 +0200

    Initialized views and wrote basic logic for saving data

--- app/src/main/java/com/rathetsu/contactsapp/AddEditContactActivity.java
+++ app/src/main/java/com/rathetsu/contactsapp/AddEditContactActivity.java
@@ -3,12 +3,53 @@ package com.rathetsu.contactsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddEditContactActivity extends AppCompatActivity {
+
+    private ImageView ivContactImage;
+    private EditText etName, etPhone, etEmail, etAddress, etBio;
+    private FloatingActionButton fabSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_contact);
+
+        ivContactImage = findViewById(R.id.ivContactImage);
+        etName = findViewById(R.id.ET_name);
+        etPhone = findViewById(R.id.ET_phone);
+        etEmail = findViewById(R.id.ET_email);
+        etAddress = findViewById(R.id.ET_address);
+        etBio = findViewById(R.id.ET_bio);
+        fabSave = findViewById(R.id.fab_save);
+
+        fabSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveContact();
+            }
+        });
+
+    }
+
+    private void saveContact() {
+        String name = etName.getText().toString();
+        String phone = etPhone.getText().toString();
+        String email = etEmail.getText().toString();
+        String address = etAddress.getText().toString();
+        String bio = etBio.getText().toString();
+
+        if (name.trim().isEmpty() || phone.trim().isEmpty()) {
+            return;
+        }
+
+        Contact contact = new Contact(name, phone, email, address, bio);
+        ContactDatabase.getInstance(this).getContactDao().insertContact(contact);
+        finish();
     }
 }
--- app/src/main/java/com/rathetsu/contactsapp/MainActivity.java
+++ app/src/main/java/com/rathetsu/contactsapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab_createContact);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
